6132b2f5f45e7bb032c5ebf8a9c53dd6628afbf7
Anddd7 <liaoad_space@sina.com>
2023-02-07 09:18:55 +0000

    custom configuration of devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+
+# zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+ZSH_PLUGINS="plugins=(git zsh-autosuggestions z)"
+
+sed -i "s/plugins=(git)/${ZSH_PLUGINS}/g" ~/.zshrc
+
+#  
