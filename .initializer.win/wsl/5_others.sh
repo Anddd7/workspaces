75b636e82ddf6b58ec176bff2bd74938034feec2
Anddd7 <liaoad_space@sina.com>
2023-11-28 10:12:07 +0800

    update configure scripts

--- .initializer.win/wsl/5_others.sh
+++ .initializer.win/wsl/5_others.sh
@@ -3,3 +3,6 @@
 # terraform cache
 mkdir -p ~/.terraform.d/plugin-cache
 echo 'plugin_cache_dir = "$HOME/.terraform.d/plugin-cache"' > ~/.terraformrc
+
+# devcontainer
+npm install -g @devcontainers/cli
